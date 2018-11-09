af3c7e66fe56b6db4b18481e1a97b999ce6337cd
LeBeke <43353818+LeBeke@users.noreply.github.com>
2018-11-09 19:18:41 +0100

    Add files via upload

--- code_ocr/image_generation/image_generation.py
+++ code_ocr/image_generation/image_generation.py
@@ -17,7 +17,7 @@ g2=random.randint(0,255)
 b2=random.randint(0,255)
 image=Image.new('RGB',(100,100),color=(r,g,b))
 ##definit la taille et lemplacement de la lettre 
-fnt = font = ImageFont.truetype("/home/niels/.local/share/fonts/arial.ttf",60)
+fnt = font = ImageFont.truetype("/home/niels/.local/share/fonts/arial.ttf",60) #changer le chemin selon le pc ;: pour lavoir entrer dans linvote de commande sous linux : fc-match -f "%{file}\n" arial
 ## ecrir la lettre sur l'image 
 d = ImageDraw.Draw(image)
 d.text((20,20),letterlist[index],font=fnt,fill=(r2,g2,b2))
